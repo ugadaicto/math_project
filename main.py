8ece4a801dd03e4c31153942dd0b15b5baa2e7fe
ugadaicto <mvsem13@yandex.ru>
2022-04-30 13:57:11 +0300

    lessons changed, final quiz added

--- main.py
+++ main.py
@@ -85,6 +85,14 @@ def insert_results_into_database(new_best, result, user_name):
     con.commit()
 
 
+def lessons(file_name):
+    c = docx.Document(file_name)
+    v = ''
+    for i in c.paragraphs[2:]:
+        v += i.text + '\n'
+    return [c.paragraphs[0].text, v]
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     global USER_LOGGED_IN
@@ -124,210 +132,76 @@ def register():
     return render_template('reg.html', title='Регистрация', form=form)
 
 
+@app.route("/quiz", methods=['GET', 'POST'])
+def quiz():
+    global LOGIN
+    global CURRENT_QUESTION
+    global ANSWERED_CORRECTLY
+    global USER_LOGGED_IN
+    global QUESTIONS
+    if USER_LOGGED_IN:
+        form = QuizForm()
+        questions = QUESTIONS
+        print(questions)
+        question = questions[CURRENT_QUESTION]
+        if form.validate_on_submit():
+            chosen_answer = None
+            if form.data["move_on"]:
+                CURRENT_QUESTION += 1
+
+                if CURRENT_QUESTION == len(questions):
+                    best_result = get_best_result_from_database(LOGIN)
+
+                    insert_results_into_database(best_result < ANSWERED_CORRECTLY * 10, ANSWERED_CORRECTLY, LOGIN)
+
+                    print(USER_LOGGED_IN)
+                    print(ANSWERED_CORRECTLY, ANSWERED_CORRECTLY / len(questions) * 100)
+                    return render_template('success.html', result=int(ANSWERED_CORRECTLY / len(questions) * 100))
+
+                question = questions[CURRENT_QUESTION]
+                return render_template('quiz.html', question=question, form=form)
+
+            if form.data["answer1"]:
+                print(CURRENT_QUESTION)
+                chosen_answer = 1
+                response, correct_answer = check_answer(question, 1)
+            if form.data["answer2"]:
+                chosen_answer = 2
+                response, correct_answer = check_answer(question, 2)
+            if form.data["answer3"]:
+                chosen_answer = 3
+                response, correct_answer = check_answer(question, 3)
+            if form.data["answer4"]:
+                chosen_answer = 4
+                response, correct_answer = check_answer(question, 4)
+
+            if response:
+                ANSWERED_CORRECTLY += 1
+                print(ANSWERED_CORRECTLY)
+
+            question = questions[CURRENT_QUESTION]
+            print(question)
+
+            return render_template('quiz.html', question=question, form=form,
+                                   correct_answer=correct_answer, chosen_answer=chosen_answer, answered=True)
+    else:
+        return render_template('menu.html', user_logged_in=USER_LOGGED_IN)
+
+    question = questions[CURRENT_QUESTION]
+
+    return render_template('quiz.html', question=question, form=form)
+
+
 @app.route('/')
 def menu():
     global USER_LOGGED_IN
     return render_template('menu.html', user_logged_in=USER_LOGGED_IN)
 
 
-@app.route("/lesson_1")
-def lesson_1():
-    return render_template("lesson.html", title='test', lesson_text='lol')
-
-
-@app.route("/lesson_2")
-def lesson_2():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_3")
-def lesson_3():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_4")
-def lesson_4():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_5")
-def lesson_5():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_6")
-def lesson_6():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_7")
-def lesson_7():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_8")
-def lesson_8():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_9")
-def lesson_9():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_10")
-def lesson_10():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_11")
-def lesson_11():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_12")
-def lesson_12():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_13")
-def lesson_13():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_14")
-def lesson_14():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_15")
-def lesson_15():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_16")
-def lesson_16():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_17")
-def lesson_17():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_18")
-def lesson_18():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_19")
-def lesson_19():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_20")
-def lesson_20():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_21")
-def lesson_21():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_22")
-def lesson_22():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_23")
-def lesson_23():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_24")
-def lesson_24():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_25")
-def lesson_25():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_26")
-def lesson_26():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_27")
-def lesson_27():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_28")
-def lesson_28():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_29")
-def lesson_29():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_30")
-def lesson_30():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_31")
-def lesson_31():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_32")
-def lesson_32():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_33")
-def lesson_33():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_34")
-def lesson_34():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_35")
-def lesson_35():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_36")
-def lesson_36():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_37")
-def lesson_37():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_38")
-def lesson_38():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_39")
-def lesson_39():
-    return render_template("lesson.html", title="", text_lesson="")
-
-
-@app.route("/lesson_40")
-def lesson_40():
-    return render_template("lesson.html", title="", text_lesson="")
+@app.route("/lesson_<int:number>/")
+def lesson(number):
+    c = lessons(f'conspects/lesson_{number}.docx')
+    return render_template("lesson.html", title=c[0], lesson_text=c[1])
 
 
 if __name__ == '__main__':
